63ff55a4c7995fe281ba6dd871d079b0d88f6539
kev <kevin.mansilla@mi.unc.edu.ar>
2021-09-16 16:38:32 -0300

    second part: vga

--- xv6/console.c
+++ xv6/console.c
@@ -15,6 +15,76 @@
 #include "proc.h"
 #include "x86.h"
 
+/////////////////////////////////////////
+#define	VGA_AC_INDEX        0x3C0
+#define	VGA_AC_WRITE        0x3C0
+#define	VGA_AC_READ         0x3C1
+#define	VGA_MISC_WRITE      0x3C2
+#define VGA_SEQ_INDEX       0x3C4
+#define VGA_SEQ_DATA        0x3C5
+#define	VGA_DAC_READ_INDEX  0x3C7
+#define	VGA_DAC_WRITE_INDEX 0x3C8
+#define	VGA_DAC_DATA        0x3C9
+#define	VGA_MISC_READ       0x3CC
+#define VGA_GC_INDEX        0x3CE
+#define VGA_GC_DATA         0x3CF
+/*			COLOR emulation		MONO emulation */
+#define VGA_CRTC_INDEX      0x3D4		/* 0x3B4 */
+#define VGA_CRTC_DATA       0x3D5		/* 0x3B5 */
+#define	VGA_INSTAT_READ     0x3DA
+
+#define	VGA_NUM_SEQ_REGS    5
+#define	VGA_NUM_CRTC_REGS   25
+#define	VGA_NUM_GC_REGS     9
+#define	VGA_NUM_AC_REGS     21
+#define	VGA_NUM_REGS        (1 + VGA_NUM_SEQ_REGS + VGA_NUM_CRTC_REGS + \
+                        VGA_NUM_GC_REGS + VGA_NUM_AC_REGS)
+
+#define	inportb(P)		inb(P)
+#define	outportb(P,V)		outb(P,V)
+
+static unsigned char g_320x200x256[] =
+{
+// MISC
+    0x63,
+// SEQ
+    0x03, 0x01, 0x0F, 0x00, 0x0E,
+// CRTC
+    0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+    0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x9C, 0x0E, 0x8F, 0x28,    0x40, 0x96, 0xB9, 0xA3,
+    0xFF,
+// GC
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
+    0xFF,
+// AC
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+    0x41, 0x00, 0x0F, 0x00,    0x00
+};
+
+static unsigned char g_80x25_text[] =
+{
+// MISC
+    0x67,
+// SEQ
+    0x03, 0x00, 0x03, 0x00, 0x02,
+// CRTC
+    0x5F, 0x4F, 0x50, 0x82, 0x55, 0x81, 0xBF, 0x1F,
+    0x00, 0x4F, 0x0D, 0x0E, 0x00, 0x00, 0x00, 0x50,
+    0x9C, 0x0E, 0x8F, 0x28, 0x1F, 0x96, 0xB9, 0xA3,
+    0xFF,
+// GC
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x0E, 0x00,
+    0xFF,
+// AC
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x14, 0x07,
+    0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+    0x0C, 0x00, 0x0F, 0x08, 0x00
+};
+
+/////////////////////////////////////////
+
 static void consputc(int);
 
 static int panicked = 0;
@@ -285,9 +355,69 @@ consolewrite(struct inode *ip, char *buf, int n)
   return n;
 }
 
-static void 
-vgainit(void)
+static void write_regs(unsigned char *regs)
 {
+  unsigned i;
+
+/* write MISCELLANEOUS reg */
+  outportb(VGA_MISC_WRITE, *regs);
+  regs++;
+/* write SEQUENCER regs */
+  for(i = 0; i < VGA_NUM_SEQ_REGS; i++)
+  {
+    outportb(VGA_SEQ_INDEX, i);
+    outportb(VGA_SEQ_DATA, *regs);
+    regs++;
+  }
+/* unlock CRTC registers */
+  outportb(VGA_CRTC_INDEX, 0x03);
+  outportb(VGA_CRTC_DATA, inportb(VGA_CRTC_DATA) | 0x80);
+  outportb(VGA_CRTC_INDEX, 0x11);
+  outportb(VGA_CRTC_DATA, inportb(VGA_CRTC_DATA) & ~0x80);
+/* make sure they remain unlocked */
+  regs[0x03] |= 0x80;
+  regs[0x11] &= ~0x80;
+/* write CRTC regs */
+  for(i = 0; i < VGA_NUM_CRTC_REGS; i++)
+  {
+    outportb(VGA_CRTC_INDEX, i);
+    outportb(VGA_CRTC_DATA, *regs);
+    regs++;
+  }
+/* write GRAPHICS CONTROLLER regs */
+  for(i = 0; i < VGA_NUM_GC_REGS; i++)
+  {
+    outportb(VGA_GC_INDEX, i);
+    outportb(VGA_GC_DATA, *regs);
+    regs++;
+  }
+/* write ATTRIBUTE CONTROLLER regs */
+  for(i = 0; i < VGA_NUM_AC_REGS; i++)
+  {
+    (void)inportb(VGA_INSTAT_READ);
+    outportb(VGA_AC_INDEX, i);
+    outportb(VGA_AC_WRITE, *regs);
+    regs++;
+  }
+/* lock 16-color palette and unblank display */
+  (void)inportb(VGA_INSTAT_READ);
+  outportb(VGA_AC_INDEX, 0x20);
+}
+
+static void
+selec_mode(int mode)
+{
+  if(mode == 1){
+    write_regs(g_320x200x256);
+  }else if (mode == 0){
+    write_regs(g_80x25_text);
+  }
+}
+
+static void
+vgainit()
+{
+  selec_mode(1);
   *(int *)P2V(0xB8F94) = 0x0353;
   *(int *)P2V(0xB8F96) = 0x034F;
   *(int *)P2V(0xB8F98) = 0x0332;
@@ -295,6 +425,8 @@ vgainit(void)
   *(int *)P2V(0xB8F9C) = 0x0332;
   *(int *)P2V(0xB8F9E) = 0x0331;
 }
+
+
 
 void
 consoleinit(void)
@@ -304,7 +436,7 @@ consoleinit(void)
   devsw[CONSOLE].write = consolewrite;
   devsw[CONSOLE].read = consoleread;
   cons.locking = 1;
+
   ioapicenable(IRQ_KBD, 0);
   vgainit();
 }
-
